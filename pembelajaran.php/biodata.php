2ca4a5a66bf6d0dab9ef7cd2bb952ab6388a79b6
aulia010709 <aulianuryulianti07@gmail.com>
2026-01-29 09:15:01 +0700

    pembelajaran.php

--- pembelajaran.php/biodata.php
+++ pembelajaran.php/biodata.php
@@ -3,6 +3,4 @@
 echo "Nama : Aulia Nur Yulianti<br>";
 echo "Jenis Kelamin : Perempuan<br>";
 echo "Umur : 16 tahun<br>";
-echo "Alamat : Jl. jelkong, Bandung<br>";
 echo "Kelas : XI TJKTT 4<br>";
-echo "Menikah : Tidak";
