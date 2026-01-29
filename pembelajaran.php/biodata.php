b76603c05d0879fc0b2440fe0746332e146d7644
aulia010709 <aulianuryulianti07@gmail.com>
2026-01-29 09:11:33 +0700

    pembelajaran.php

---
+++ pembelajaran.php/biodata.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "Nama : Aulia Nur Yulianti<br>";
+echo "Jenis Kelamin : Perempuan<br>";
+echo "Umur : 16 tahun<br>";
+echo "Alamat : Jl. jelkong, Bandung<br>";
+echo "Kelas : XI TJKTT 4<br>";
+echo "Menikah : Tidak";
--- pembelajaran.php/variabel.php
+++ pembelajaran.php/variabel.php
@@ -1,8 +1,35 @@
 <?php
 
-// membuat variabel baru
-$nama_barang = "Mie kuah";
-$harga = 3500;
+// tipe data char (karakter)
+$jenis_kelamin = 'P';
 
-// menampilkan isi variabel
-echo "Aulia membeli $nama_barang seharga Rp $harga";
+// tipe data string (teks)
+$nama_lengkap = "Aulia Nur Yulianti";
+
+// tipe data integer
+$umur = 16;
+
+// tipe data float
+$berat = 60.3;
+
+// tipe data float
+$tinggi = 162.2;
+
+// tipe data maps
+$alamat = "Jl. jelkong, Bandung";
+
+// tipe data string (teks)
+$kelas = " XI TJKTT 4";
+
+// tipe data boolean
+$menikah = false;
+
+
+echo "Nama: $nama_lengkap<br>";
+echo "Jenis Kelamin: $jenis_kelamin<br>";
+echo "Umur: $umur tahun<br>";
+echo "berat badan: $berat kg<br>";
+echo "tinggi badan: $tinggi cm<br>";
+echo "alamat: $alamat <br>";
+echo "kelas: $kelas <br>";
+echo "menikah: $menikah";
