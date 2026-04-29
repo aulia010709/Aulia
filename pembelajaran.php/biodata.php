38c486dd2a48edbd6ad012e2b7ff31bc3bd67d6d
aulia010709 <aulianuryulianti07@gmail.com>
2026-04-29 09:07:25 +0700

    pembelajaran.php

--- pembelajaran.php/biodata.php
+++ pembelajaran.php/biodata.php
@@ -1,6 +1,34 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Biodata Ekskul Seni</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
 
-echo "Nama : Aulia Nur Yulianti<br>";
-echo "Jenis Kelamin : Perempuan<br>";
-echo "Umur : 16 tahun<br>";
-echo "Kelas : XI TJKT 4<br>";
+<h2>FORM BIODATA PENDAFTAR EKSKUL SENI</h2>
+
+<table border="1">
+    <tr>
+        <th>No</th>
+        <th>Nama</th>
+        <th>Kelas</th>
+        <th>NIS</th>
+        <th>Alamat</th>
+        <th>Tempat Lahir</th>
+        <th>Tanggal Lahir</th>
+    </tr>
+
+    <tr>
+        <td><input type="text"></td>
+        <td><input type="text"></td>
+        <td><input type="text"></td>
+        <td><input type="text"></td>
+        <td><input type="text"></td>
+        <td><input type="text"></td>
+        <td><input type="date"></td>
+    </tr>
+</table>
+
+</body>
+</html>
