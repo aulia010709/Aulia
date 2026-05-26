287c4399370a1b80750a2d187bd6570609e25b85
aulia010709 <aulianuryulianti07@gmail.com>
2026-05-26 12:26:46 +0700

    update.php

--- crud/koneksi.php
+++ crud/koneksi.php
@@ -1,7 +1,7 @@
 <?php
 $host = "localhost";
-$user = "root";
-$pass = "";
+$user = "2526_25";
+$pass = "12345678";
 $db   = "2526_25db"; // Menggunakan nama database asli di komputermu
 
 $koneksi = mysqli_connect($host, $user, $pass, $db);
