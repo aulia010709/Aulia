82a4bb7b8f019bf2f07f471dfbcd8a2494a8a75e
aulia010709 <aulianuryulianti07@gmail.com>
2026-06-01 14:03:18 +0700

    update

--- crud/koneksi.php
+++ crud/koneksi.php
@@ -1,12 +1,10 @@
 <?php
-$host = "localhost";
-$user = "2526_25";
-$pass = "12345678";
-$db   = "2526_25db"; // Menggunakan nama database asli di komputermu
+// Sesuaikan isi di dalam tanda kurung dengan data servermu
+$koneksi = mysqli_connect("localhost", "username_kamu", "password_kamu", "nama_database_kamu");
 
-$koneksi = mysqli_connect($host, $user, $pass, $db);
-
-if (!$koneksi) {
-    die("Koneksi database gagal: " . mysqli_connect_error());
+// TAMBAHKAN KODE INI DI BAWAHNYA:
+if (mysqli_connect_errno()) {
+    echo "Gagal terhubung ke database: " . mysqli_connect_error();
+    exit();
 }
 ?>
